e304da00a369d78c59e080c1e0c62207d872cf94
choewy <choewy32@gmail.com>
2022-04-09 00:28:08 +0900

    9시간 추가(임시)

--- server/src/_util/date-formatter.util.js
+++ server/src/_util/date-formatter.util.js
@@ -6,7 +6,7 @@ const dateFormat = (date) => {
         dt.getFullYear(), "-",
         `0${dt.getMonth() + 1}`.slice(-2) + "-",
         `0${dt.getDate()}`.slice(-2), " ",
-        `0${dt.getHours()}`.slice(-2), ":",
+        `0${dt.getHours() + 9}`.slice(-2), ":",
         `0${dt.getMinutes()}`.slice(-2)
     ].join('');
 };
