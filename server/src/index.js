e11900de14046dfd73b3024e11ad0634bb2abd73
choewy <choewy32@gmail.com>
2022-04-08 13:59:14 +0900

    index.js → ./src/index.js

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config();
 
-const app = require('./src/_app/app.module');
+const app = require('./_app/app.module');
 const port = process.env.NODE_PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
